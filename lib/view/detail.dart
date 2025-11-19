6c1b8c4f550e81356a24c2f8e7c4e8f30253b0ce
KorWeiss <signalizer@hanmail.net>
2025-11-19 12:52:17 +0900

    ㅇㅇ

--- lib/view/detail.dart
+++ lib/view/detail.dart
@@ -11,24 +11,24 @@ class Detail extends StatefulWidget {
 }
 
 class _DetailState extends State<Detail> {
-  late Datastorage detailFlower;
+  late Datastorage detailFlower;    //  넘어온 argument 받아올 변수
 
   @override
   void initState() {
     super.initState();
-    detailFlower = Get.arguments;
+    detailFlower = Get.arguments;   //  Get.argument를 사용하여 넘어온 변수 받아오기
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: restiApB(
-        appBarText: detailFlower.flowerName,
+        appBarText: detailFlower.flowerName,    //  넘어온 꽃 이름으로 타이틀 설정
         align: AppbarAlign.c,
       ),
       body: Center(
         child: Image.asset(
-          detailFlower.imagePath,
+          detailFlower.imagePath,   //  넘어온 꽃 imagePath를 이용하여 이미지 표기
           width: 150,
           fit: BoxFit.contain,
         ),
